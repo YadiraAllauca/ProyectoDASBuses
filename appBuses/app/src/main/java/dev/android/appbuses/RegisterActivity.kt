279a28fdbcba8b597bfc5bfd230b92f95a25886f
Keybrish <keychanpa@gmail.com>
2023-06-03 12:28:49 -0500

    Interfaz Registro

--- appBuses/app/src/main/java/dev/android/appbuses/RegisterActivity.kt
+++ appBuses/app/src/main/java/dev/android/appbuses/RegisterActivity.kt
@@ -4,21 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
-import dev.android.appbuses.databinding.ActivityWelcomeBinding
+import dev.android.appbuses.databinding.ActivityRegisterBinding
 
-class WelcomeActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityWelcomeBinding
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(binding.root)
 
-        binding.btnRegister.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java).apply {
-            }
-            startActivity(intent)
+        binding.btnBack.setOnClickListener {
+            finish()
         }
+
     }
 }
