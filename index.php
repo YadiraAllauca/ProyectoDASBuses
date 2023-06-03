75a64e938ff453f650b8b98082109edc099b66d2
diana9519 <94394943+diana9519@users.noreply.github.com>
2023-06-03 15:22:44 -0500

    Interfaz Home

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SAFEY!- Login</title>
+    <link rel="stylesheet" href="bootstrap-5.2.0-beta1-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+
+</head>
+
+
+<body class="bodyBack">
+    <header class="headerStyle">
+        <div style="color:white">
+            <img class="avatar" src="img/safey.png">.....--------------------------------------------.........-------------------------------------------------------------------------------------------------.....................
+            <button type="button" class="btn buttonStyle" id="btnLogin">Login</button>
+        </div>
+    </header>
+    <div class="indexStyle"> 
+    <br><br><br><br>    
+    <p>Welcome to <img class="avatar" src="img/safey.png"></p>
+        
+    <p>With safety you can travel safely to all your favorite destinations!</p></div>
+   
+
+    <footer class="footerStyle">
+        <p>
+            <br>
+            <b>______________________________________</b>
+            <b>______________________________________</b>
+            <br>
+            <b>Copyright © 2023 Safey</b>
+            <br>
+            <b><a href="">Home</a> | <a href="">About</a></b>
+        </p>
+    </footer>
+</body>
+
+</html>
---
+++ main.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SAFEY!- Login</title>
+    <link rel="stylesheet" href="bootstrap-5.2.0-beta1-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+</head>
+
+
+
+<body class="bodyBack">
+    
+    <header class="headerStyle">
+        <div style=" display: flex; justify-content: space-between;">
+            <img class="avatar" src="img/safey.png">
+            <button type="button" class="btn buttonStyle" id="btnLogin">Login</button>
+        </div>
+        <nav class="navbar-expand-lg navbar-dark menuStyle custom-nav">>
+            <div class="container-fluid">
+                <a class="navbar-brand" href="redireccion.php?action=home">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="redireccion.php?action=invernaderos">Cooperatives</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="redireccion.php?action=registrarinvernadero" style="margin-right: 20px;">Frequencies</a>
+                        </li>
+                    </ul>
+                </a>
+            </div>
+        </nav>
+
+
+
+
+        
+    </header>
+    <div class="indexStyle"> 
+    <br><br><br><br>    
+    <p>Welcome to <img class="avatar" src="img/safey.png"></p>
+        
+    <p>With safety you can travel safely to all your favorite destinations!</p></div>
+   
+
+    <footer class="footerStyle">
+        <p>
+            <br>
+            <b>______________________________________</b>
+            <b>______________________________________</b>
+            <br>
+            <b>Copyright © 2023 Safey</b>
+            <br>
+            <b><a href="">Home</a> | <a href="">About</a></b>
+        </p>
+    </footer>
+</body>
+
+</html>
