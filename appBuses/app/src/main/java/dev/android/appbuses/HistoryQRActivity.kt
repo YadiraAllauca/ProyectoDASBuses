37e9db48e3fe17a76188f0d2eedfd63c4d5cc227
Keybrish <keychanpa@gmail.com>
2023-06-04 11:05:22 -0500

    Interfaz Historial Código QR

--- appBuses/app/src/main/java/dev/android/appbuses/HistoryQRActivity.kt
+++ appBuses/app/src/main/java/dev/android/appbuses/HistoryQRActivity.kt
@@ -1,28 +1,21 @@
 package dev.android.appbuses
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
-import dev.android.appbuses.databinding.ActivityHistoryBinding
+import dev.android.appbuses.databinding.ActivityHistoryQractivityBinding
 
-class HistoryActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityHistoryBinding
+class HistoryQRActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityHistoryQractivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHistoryBinding.inflate(layoutInflater)
+        binding = ActivityHistoryQractivityBinding.inflate(layoutInflater)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(binding.root)
 
         binding.btnBack.setOnClickListener {
             finish()
-        }
-
-        binding.textView30.setOnClickListener {
-            val intent = Intent(this, HistoryQRActivity::class.java).apply {
-            }
-            startActivity(intent)
         }
 
     }
