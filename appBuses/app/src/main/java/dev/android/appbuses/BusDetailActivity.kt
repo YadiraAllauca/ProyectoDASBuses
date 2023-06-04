0edda2483458ca861a03a911ede4e5a9f8a47cd8
Keybrish <keychanpa@gmail.com>
2023-06-03 20:36:49 -0500

    Interfaz Pago Info

--- appBuses/app/src/main/java/dev/android/appbuses/BusDetailActivity.kt
+++ appBuses/app/src/main/java/dev/android/appbuses/BusDetailActivity.kt
@@ -1,14 +1,21 @@
 package dev.android.appbuses
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.view.Gravity
+import android.view.Window
+import dev.android.appbuses.databinding.ActivityBusDetailBinding
 
 class BusDetailActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityBusDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bus_detail)
+        binding = ActivityBusDetailBinding.inflate(layoutInflater)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(binding.root)
 
         val window = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(window)
@@ -18,5 +25,11 @@ class BusDetailActivity : AppCompatActivity() {
         getWindow().setLayout(((width * 1)), 1400)
         getWindow().decorView.setBackgroundResource(android.R.color.transparent)
         getWindow().setGravity(Gravity.BOTTOM)
+
+        binding.btnBuy.setOnClickListener {
+            val intent = Intent(this, PaymentActivity::class.java).apply {
+            }
+            startActivity(intent)
+        }
     }
 }
---
+++ appBuses/app/src/main/java/dev/android/appbuses/PaymentActivity.kt
@@ -0,0 +1,33 @@
+package dev.android.appbuses
+
+import android.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.widget.ArrayAdapter
+import dev.android.appbuses.databinding.ActivityPaymentBinding
+
+class PaymentActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityPaymentBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPaymentBinding.inflate(layoutInflater)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(binding.root)
+
+        binding.spnPayment.dropDownVerticalOffset
+        val op = listOf("Paypal", "Deposito", "Transferencia")
+        val sp = ArrayAdapter(this, R.layout.simple_spinner_item, op)
+        binding.spnPayment.adapter = sp
+
+        binding.btnLess.setOnClickListener {
+            if(binding.txtAmount.text.toString().toInt() > 0)
+                binding.txtAmount.text = (binding.txtAmount.text.toString().toInt() - 1).toString()
+        }
+
+        binding.btnPlus.setOnClickListener {
+            binding.txtAmount.text = (binding.txtAmount.text.toString().toInt() + 1).toString()
+        }
+    }
+}
