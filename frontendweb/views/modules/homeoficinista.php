475b5db1ae6fe4833ccffd1f758e9139c926db13
Eldinosaur <82054019+Eldinosaur@users.noreply.github.com>
2023-06-05 16:29:42 -0500

    Revert "Merge changes frontendweb main with frontendwebofi"

--- frontendweb/views/modules/homeoficinista.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="css/styles.css">
-<div class="indexStyle">
-    <br><br><br><br>
-    <p>Welcome to <img class="avatar" src="img/safey.jpg"></p>
-
-    <p>With safety you can travel safely to all your favorite destinations!</p>
-</div>
