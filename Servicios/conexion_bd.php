6bebe19fa598af7fc38b3182b77bae18dc47e7ad
YadiraAllauca <yallauca3770@uta.edu.ec>
2023-06-04 20:27:47 -0500

    primera conexión a la base de datos

---
+++ Servicios/conexion_bd.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost";
+$dbUsuario = "id20780986_gcode"; 
+$dbContraseña = "gcodeDAS1."; 
+$dbNombre = "id20780986_proyecto_buses_das";
+
+$conexion = new mysqli($host, $dbUsuario, $dbContraseña, $dbNombre);
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}else{
+  echo "Conexión exitosa a la base de datos";
+}
+
+$conexion->close();
+?>
