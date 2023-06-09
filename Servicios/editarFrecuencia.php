ef87235e335ffb20669c5cf01634e288249842fc
YadiraAllauca <yallauca3770@uta.edu.ec>
2023-06-09 14:51:52 -0500

    servicio editar frecuencia 01

---
+++ Servicios/editarFrecuencia.php
@@ -0,0 +1,26 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-with, Content-type, Authorization');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+
+include 'conexionBDRemota.php';
+
+$id_frecuencia =  $_POST['id_frecuencia'];
+$origen_frecuencia =  $_POST['origen_frecuencia'];
+$destino_frecuencia =  $_POST['destino_frecuencia'];
+$duracion_frecuencia = $_POST['duracion_frecuencia'];
+$clave_usuario = $_POST['clave_usuario'];
+$costo_frecuencia = $_POST['costo_frecuencia'];
+$estado_frecuencia = $_POST['estado_frecuencia'];
+
+$sql = "UPDATE Frecuencias SET origen_frecuencia='$origen_frecuencia', destino_frecuencia='$destino_frecuencia', duracion_frecuencia='$duracion_frecuencia', apellido_usuario='$apellido_usuario', costo_frecuencia='$costo_frecuencia', estado_frecuencia='$estado_frecuencia' WHERE id_frecuencia='$id_frecuencia'";
+
+if ($conexion->query($sql)===TRUE) {
+    echo json_encode(array('OK'=>TRUE));
+} else {
+    echo json_encode(array('OK'=>FALSE, 'errorMsg'=>$sql.$conexion->error));
+}
+
+$conexion->close();
+?>
