770310554587e201695ecf228f431fe1c1cd4091
diana9519 <94394943+diana9519@users.noreply.github.com>
2023-06-13 18:55:17 -0500

    ventana emergente de guardado exitosamente frecuencias

--- frontendweb/frontwebadmin/views/modules/frecuenciasformadmin.php
+++ frontendweb/frontwebadmin/views/modules/frecuenciasformadmin.php
@@ -13,6 +13,16 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="script.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/js/bootstrap.bundle.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $("form").submit(function(event) {
+                event.preventDefault(); // Evitar que se envíe el formulario
+
+                // Mostrar ventana emergente con mensaje y icono
+                $('#successModal').modal('show');
+            });
+        });
+    </script>
 </head>
 
 <body class="bodyBack">
@@ -155,6 +165,23 @@
             <b><a href="index.php" style="color:white">Home</a> | <a href="" style="color:white">About</a></b>
         </p>
     </footer>
+    <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body text-center">
+                    <i class="fas fa-check-circle fa-4x text-success"></i>
+                    <p class="mt-3">Frequency saved successfully</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Ok</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
