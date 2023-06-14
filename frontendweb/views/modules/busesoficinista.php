296dee4153b8d0397052b3ff089525557b46f9a2
Eldinosaur <eldinosaur7u7@outlook.com>
2023-06-14 10:52:57 -0500

    agregacion de opcion "NewBus" del modulo de buses

--- frontendweb/views/modules/busesoficinista.php
+++ frontendweb/views/modules/busesoficinista.php
@@ -1,18 +1,25 @@
 <div style="padding-left: 30px; padding-right: 30px; padding-top: 15px;">
-<table class="table table-striped">
-<thead>
-<tr>
-    <th scope="col">Plate Number</th>
-    <th scope="col">Owner</th>
-    <th scope="col">Actions</th>
-</tr>
-</thead>
-  <tbody>
-    <tr>
-        <td colspan="3"><center>No buses registrated</center></td>
-    </tr>
-    
-</tbody>
-</table>
+    <a class="nav-link active; navTemplate" href="redireccionoficinista.php?action=buses">
+        New Bus
+        <img src="img/plus.png" class="icons">
+    </a>
+    <br>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Plate Number</th>
+                <th scope="col">Owner</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td colspan="3">
+                    <center>No buses registrated</center>
+                </td>
+            </tr>
+
+        </tbody>
+    </table>
 
 </div>
