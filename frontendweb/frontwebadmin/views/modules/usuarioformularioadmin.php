356d084a2f2f0a7b03741e683e751c2fb39a95d9
diana9519 <94394943+diana9519@users.noreply.github.com>
2023-06-13 21:32:29 -0500

    Ventana emergente de Guardado exitosamente

--- frontendweb/frontwebadmin/views/modules/usuarioformularioadmin.php
+++ frontendweb/frontwebadmin/views/modules/usuarioformularioadmin.php
@@ -13,7 +13,16 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="script.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/js/bootstrap.bundle.min.js"></script>
-    
+    <script>
+        $(document).ready(function() {
+            $("form").submit(function(event) {
+                event.preventDefault(); // Evitar que se envíe el formulario
+
+                // Mostrar ventana emergente con mensaje y icono
+                $('#successModal').modal('show');
+            });
+        });
+    </script>
 </head>
 
 <body class="bodyBack">
@@ -97,7 +106,23 @@
         </div>
 
     </div>
-    
+    <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body text-center">
+                    <i class="fas fa-check-circle fa-4x text-success"></i>
+                    <p class="mt-3">Saved successfully</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Ok</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
