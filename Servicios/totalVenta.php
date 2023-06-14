01efea8c9718409106f8a0430602939ac18f2693
YadiraAllauca <yallauca3770@uta.edu.ec>
2023-06-14 08:06:45 -0500

    ingresar Detalle venta actualizado

---
+++ Servicios/totalVenta.php
@@ -0,0 +1,10 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-with, Content-type, Authorization');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+
+include 'conexionBDRemota.php';
+
+$sql = "SELECT ";
+?>
