2fb8d7daae4f4a9b97dee671afac796b6914354a
Keybrish <keychanpa@gmail.com>
2023-06-14 17:14:23 -0500

    Interfaz Inicio Sesión

--- appBusesDriver/app/src/main/java/dev/android/appbusesdriver/LoginActivity.kt
+++ appBusesDriver/app/src/main/java/dev/android/appbusesdriver/LoginActivity.kt
@@ -4,19 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
-import dev.android.appbusesdriver.databinding.ActivityWelcomeBinding
+import dev.android.appbusesdriver.databinding.ActivityLoginBinding
 
-class WelcomeActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityWelcomeBinding
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(binding.root)
 
-        binding.btnLogin.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java).apply {
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
+
+        binding.btnNext.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java).apply {
             }
             startActivity(intent)
         }
