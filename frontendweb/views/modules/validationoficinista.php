071282a819503240089e2c3070a0ec275022731d
Eldinosaur <eldinosaur7u7@outlook.com>
2023-06-14 10:34:49 -0500

    formato tabla de modulo de validacion

---
+++ frontendweb/views/modules/validationoficinista.php
@@ -0,0 +1,18 @@
+<div style="padding-left: 30px; padding-right: 30px; padding-top: 15px;">
+<table class="table table-striped">
+<thead>
+<tr>
+    <th scope="col">Order Number</th>
+    <th scope="col">Status</th>
+    <th scope="col">Actions</th>
+</tr>
+</thead>
+  <tbody>
+    <tr>
+        <td colspan="3"><center>No sales pending validation</center></td>
+    </tr>
+    
+</tbody>
+</table>
+
+</div>
