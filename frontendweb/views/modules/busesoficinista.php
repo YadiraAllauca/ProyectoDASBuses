6456de48e449ac044f7a36fd48eeb41fe740f8ea
Eldinosaur <eldinosaur7u7@outlook.com>
2023-06-16 10:10:18 -0500

    traduccion de modulo de buses

--- frontendweb/views/modules/busesoficinista.php
+++ frontendweb/views/modules/busesoficinista.php
@@ -1,21 +1,21 @@
 <div style="padding-left: 30px; padding-right: 30px; padding-top: 15px;">
     <a class="nav-link active; navTemplate" href="redireccionoficinista.php?action=newbus">
-        New Bus
+        Nuevo Bus
         <img src="img/plus.png" class="icons">
     </a>
     <br>
     <table class="table table-striped">
         <thead>
             <tr>
-                <th scope="col">Plate Number</th>
-                <th scope="col">Owner</th>
-                <th scope="col">Actions</th>
+                <th scope="col">Numero de Placa</th>
+                <th scope="col">Dueño</th>
+                <th scope="col">Acciones</th>
             </tr>
         </thead>
         <tbody>
             <tr>
                 <td colspan="3">
-                    <center>No buses registrated</center>
+                    <center>No existen buses registrados</center>
                 </td>
             </tr>
 
