e2454ece7452609c30b6447088045ad708c2a05c
YadiraAllauca <yallauca3770@uta.edu.ec>
2023-06-17 16:57:42 -0500

    actualización servicio agregar cooperativa para que edite también si lo requiere

--- Servicios/agregarCooperativa.php
+++ Servicios/agregarCooperativa.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ERROR | E_PARSE);
+
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Origin, X-Requested-with, Content-type, Authorization');
 header('Content-Type: application/json');
@@ -6,12 +8,18 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 
 include 'conexionBDRemota.php';
 
+
+$id_cooperativa = $_POST['id_cooperativa'];
 $ruc_cooperativa = $_POST['ruc_cooperativa'];
 $nombre_cooperativa = $_POST['nombre_cooperativa'];
 $cantidad_buses = $_POST['cantidad_buses'];
 
-$sql = "INSERT INTO Cooperativas (ruc_cooperativa, nombre_cooperativa, cantidad_buses) VALUES ('$ruc_cooperativa', '$nombre_cooperativa', '$cantidad_buses')";
+if (empty($id_cooperativa)) {
+  $sql = "INSERT INTO Cooperativas (ruc_cooperativa, nombre_cooperativa, cantidad_buses) VALUES ('$ruc_cooperativa', '$nombre_cooperativa', '$cantidad_buses')";
+}else{
+  $sql = "UPDATE Cooperativas SET ruc_cooperativa = '$ruc_cooperativa', nombre_cooperativa = '$nombre_cooperativa', cantidad_buses = '$cantidad_buses' WHERE id_cooperativa = $id_cooperativa";
 
+}
 if ($conexion->query($sql)===TRUE) {
     echo json_encode(array('OK'=>TRUE));
 } else {
@@ -19,4 +27,3 @@ if ($conexion->query($sql)===TRUE) {
 }
 
 $conexion->close();
-?>
