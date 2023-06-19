71f94bf4db688b20bff45a61670606254bf670db
diana9519 <94394943+diana9519@users.noreply.github.com>
2023-06-19 18:19:07 -0500

    consumo del servicio para editar Frecuencias

--- frontendweb/frontwebadmin/views/modules/formularios/frecuenciasformadmin.php
+++ frontendweb/frontwebadmin/views/modules/formularios/frecuenciasformadmin.php
@@ -1,125 +1,118 @@
 <head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/js/bootstrap.bundle.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $("form").submit(function(event) {
-                event.preventDefault(); // Evitar que se envíe el formulario
+<script>
+    function editarFrecuencia(id_frecuencia, origen_frecuencia, destino_frecuencia, costo_frecuencia, duracion_frecuencia) {
+      var encodedId = encodeURIComponent(id_frecuencia);
+      var encodedOrigen = encodeURIComponent(origen_frecuencia);
+      var encodedDestino = encodeURIComponent(destino_frecuencia);
+      var encodedCosto = encodeURIComponent(costo_frecuencia);
+      var encodedDuracion = encodeURIComponent(duracion_frecuencia);
+      var redirectUrl = 'redireccionadmin.php?action=frecuenciasform&id_frecuencia=' + encodedId +
+        '&origen_frecuencia=' + encodedOrigen +
+        '&destino_frecuencia=' + encodedDestino +
+        '&costo_frecuencia=' + encodedCosto+
+        '&duracion_frecuencia=' + encodedDuracion;
 
-                // Mostrar ventana emergente con mensaje y icono
-                $('#successModal').modal('show');
-            });
-        });
-    </script>
+      window.location.href = redirectUrl;
+    }
+  </script>
 </head>
-
 <body>
     <div class="indexStyleTitulo">
+        <br><br><br>
         <div style="padding-left: 30px; padding-right: 30px; padding-top: 15px;">
-            <h3 style="font-size: 20px; text-align: center;">
-                <span class="cooperative-info-title">FREQUENCY INFO!</span>
+            <h3 style="font-size: 20px;">
+                <span style="display: inline-block; vertical-align: middle;">Nueva Frecuencia</span>
+                <a href="redireccionadmin.php?action=frecuenciasform">
+                    <img class="iconos" src="img/mas.png">
+                </a>
             </h3>
-            <form style="max-width: 400px; margin: 0 auto;">
-                <div class="row mb-3">
-                    <label for="origin" class="col-sm-4 col-form-label">Origin:</label>
-                    <div class="col-sm-8">
-                        <input type="text" class="form-control form-control-sm" id="origin" placeholder="Enter origin" style="height: auto; padding: 0.375rem 0.75rem; font-size: 0.875rem;">
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <label for="destination" class="col-sm-4 col-form-label">Destination:</label>
-                    <div class="col-sm-8">
-                        <input type="text" class="form-control form-control-sm" id="destination" placeholder="Enter destination" style="height: auto; padding: 0.375rem 0.75rem; font-size: 0.875rem;">
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <label for="cost" class="col-sm-4 col-form-label">Cost:</label>
-                    <div class="col-sm-8">
-                        <input type="text" class="form-control form-control-sm" id="cost" placeholder="Enter cost" style="height: auto; padding: 0.375rem 0.75rem; font-size: 0.875rem;">
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <label for="duration" class="col-sm-4 col-form-label">Duration:</label>
-                    <div class="col-sm-8">
-                        <input type="text" class="form-control form-control-sm" id="duration" placeholder="Enter duration" style="height: auto; padding: 0.375rem 0.75rem; font-size: 0.875rem;">
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <label for="state" class="col-sm-4 col-form-label">State:</label>
-                    <div class="col-sm-8">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="state" value="active">
-                            <label class="form-check-label" for="state">
-                                Active
-                            </label>
-                        </div>
-                    </div>
-                </div>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Origen</th>
+                        <th scope="col">Destino</th>
+                        <th scope="col">Costo</th>
+                        <th scope="col">Duración</th>
+                        <th scope="col">Acciones </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $url = 'https://nilotic-quart.000webhostapp.com/listarFrecuencias.php';
 
-                <div class="d-flex justify-content-between">
-                    <button type="submit" class="btn btn-primary" style="width: 45%;">Save</button>
-                    <button type="button" class="btn btn-outline-primary" style="width: 45%;">Cancel</button>
-                </div>
-            </form>
-        </div>
+                        $ch = curl_init($url);
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-    </div>
-    <div class="modal fade" id="frequencyModal" tabindex="-1" aria-labelledby="frequencyModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="frequencyModalLabel">Add Frequency</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <table id="frequencyTable" class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th scope="col">Origin</th>
-                                <th scope="col">Destination</th>
-                                <th scope="col">Duration</th>
-                                <th scope="col">Cost</th>
-                                <th scope="col">State</th>
-                                <th scope="col"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>&nbsp;</td>
-                                <td>&nbsp;</td>
-                                <td>&nbsp;</td>
-                                <td>&nbsp;</td>
-                                <td>&nbsp;</td>
-                                <td>
-                                    <input type="checkbox">
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary">Save</button>
-                    <button type="button" class="btn btn-outline-primary" data-bs-dismiss="modal">Close</button>
-                </div>
-            </div>
-        </div>
-    </div>
+                        $response = curl_exec($ch);
 
-    <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
+                        if ($response !== false) {
+                            $data = json_decode($response, true);
 
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body text-center">
-                    <i class="fas fa-check-circle fa-4x text-success"></i>
-                    <p class="mt-3">Saved successfully</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Ok</button>
-                </div>
-            </div>
+                            if (!empty($data)) {
+                                echo '<script>
+                                    var cityMap = {
+                                      1: "Cuenca",
+                                      2: "Guaranda",
+                                      3: "Azogues",
+                                      4: "Tulcán",
+                                      5: "Riobamba",
+                                      6: "Latacunga",
+                                      7: "Machala",
+                                      8: "Esmeraldas",
+                                      9: "Guayaquil",
+                                      10: "Ibarra",
+                                      11: "Loja",
+                                      12: "Babahoyo",
+                                      13: "Portoviejo",
+                                      14: "Macas",
+                                      15: "Tena",
+                                      16: "Orellana",
+                                      17: "Puyo",
+                                      18: "Quito",
+                                      19: "Santa Elena",
+                                      20: "Santo Domingo",
+                                      21: "Nueva Loja",
+                                      22: "Ambato",
+                                      23: "Zamora",
+                                      24: "Salcedo",
+                                      25: "La Maná"
+                                    };
+
+                                    $(document).ready(function() {
+                                      var cities = $(".city");
+
+                                      cities.each(function() {
+                                        var cityId = $(this).data("id");
+                                        var cityName = cityMap[cityId];
+                                        $(this).text(cityName);
+                                      });
+
+                        
+                                    });
+                                </script>';
+
+                                foreach ($data as $frecuencia) {
+                                    echo '<tr>';
+                                    echo '<td class="city" data-id="' . $frecuencia['origen_frecuencia'] . '"></td>';
+                                    echo '<td class="city" data-id="' . $frecuencia['destino_frecuencia'] . '"></td>';
+                                    echo '<td>' . $frecuencia['costo_frecuencia'] . '</td>';
+                                    echo '<td>' . $frecuencia['duracion_frecuencia'] . '</td>';
+                                    echo '<td>';
+                                    echo '<img class="iconos" src="img/editar.png" onclick="editarFrecuencia(\'' . $frecuencia['id_frecuencia'] . '\', \'' . $frecuencia['origen_frecuencia'] . '\', \'' . $frecuencia['destino_frecuencia'] . '\', \'' . $frecuencia['costo_frecuencia'] . '\',\'' . $frecuencia['duracion_frecuencia'] . '\')">';
+                                    echo '<img class="iconos" src="img/borrar.png">';
+                                    echo '</td>';
+                                    echo '</tr>';
+                                }
+                            } else {
+                                echo '<tr><td colspan="5">No se encontraron registros en la tabla</td></tr>';
+                            }
+                        } else {
+                            echo '<tr><td colspan="5">Error al obtener los datos</td></tr>';
+                        }
+                        curl_close($ch);
+                    ?>
+                </tbody>
+            </table>
         </div>
     </div>
 </body>
