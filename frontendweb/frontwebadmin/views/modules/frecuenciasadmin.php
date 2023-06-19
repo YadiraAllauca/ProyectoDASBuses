edb9f03236f9b79f7dd1b204936ab20002a9b769
diana9519 <94394943+diana9519@users.noreply.github.com>
2023-06-19 16:00:26 -0500

    combo box de origen y destino de frecuencias

--- frontendweb/frontwebadmin/views/modules/frecuenciasadmin.php
+++ frontendweb/frontwebadmin/views/modules/frecuenciasadmin.php
@@ -2,6 +2,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    
     <script>
         $(document).ready(function() {
             // Realizar la solicitud AJAX al servicio
