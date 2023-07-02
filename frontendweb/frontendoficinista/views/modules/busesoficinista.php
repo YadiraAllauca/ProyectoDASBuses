2c39c2f408a2461ff6e98114bc2efa020f420efe
Eldinosaur <eldinosaur7u7@outlook.com>
2023-07-01 20:34:44 -0500

    funcional update

--- frontendweb/frontendoficinista/views/modules/busesoficinista.php
+++ frontendweb/frontendoficinista/views/modules/busesoficinista.php
@@ -36,7 +36,9 @@
                     $id_socio = $val[$i]['id_socio'];
                     $estado = $val[$i]['estado'];
                     ?>
-                    <tr>
+                    <tr><td>
+                            <?php echo $id_bus; ?>
+                        </td>
                         <td>
                             <?php echo $placa_bus; ?>
                         </td>
@@ -57,10 +59,7 @@
                             } ?>
                         </td>
                         <td><a class="nav-link active; navTemplate"
-                                href="redireccionoficinista.php?action=updatebus&id_bus=<?php echo $id_bus ?>
-                        &numero_bus=<?php echo $numero_bus ?>&placa_bus=<?php echo $placa_bus ?>
-                        &carroceria_bus=<?php echo $carroceria_bus ?>&cantidad_asientos=<?php echo $cantidad_asientos ?>
-                        &fotografia=<?php echo $fotografia ?>&id_socio=<?php echo $id_socio ?>&estado=<?php echo $estado ?>">
+                                href="redireccionoficinista.php?action=updatebus&id_bus=<?php echo $id_bus; ?>&numero_bus=<?php echo $numero_bus; ?>&placa_bus=<?php echo $placa_bus; ?>&chasis_bus=<?php echo $chasis_bus;?>&carroceria_bus=<?php echo $carroceria_bus; ?>&cantidad_asientos=<?php echo $cantidad_asientos; ?>&fotografia=<?php echo $fotografia; ?>&id_socio=<?php echo $id_socio; ?>&estado=<?php echo $estado; ?>">
                                 <img src="img/editar.png" class="icons">
                             </a></td>
                     </tr>
--- frontendweb/frontendoficinista/views/modules/updatebusoficinista.php
+++ frontendweb/frontendoficinista/views/modules/updatebusoficinista.php
@@ -1,7 +1,39 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+  $(document).ready(function() {
+    $('#newBus').submit(function(e) {
+      e.preventDefault(); // Prevent the default form submission
+
+      // Retrieve form data
+      var formData = $(this).serialize();
+
+      // Send the form data using AJAX
+      $.ajax({
+        type: 'POST',
+        url: "https://nilotic-quart.000webhostapp.com/agregarBus.php",
+        data: formData,
+        success: function(response) {
+          console.log(response);
+        },
+        error: function(xhr, status, error) {
+          // Handle the error case
+          console.log(xhr.responseText); // Example: Log the error response to the browser console
+        }
+      });
+    });
+  });
+</script>
+<script>
+  function redirectToBuses(){
+    window.location.href = 'redireccionoficinista.php?action=buses';
+  }
+</script>
+
 <?php
 $id_bus = $_GET['id_bus'];
 $numero_bus = $_GET['numero_bus'];
 $placa_bus = $_GET['placa_bus'];
+$chasis_bus = $_GET['chasis_bus'];
 $carroceria_bus = $_GET['carroceria_bus'];
 $cantidad_asientos = $_GET['cantidad_asientos'];
 $fotografia = $_GET['fotografia'];
@@ -18,11 +50,11 @@ $estado = $_GET['estado'];
 
 <body class="bodyBack">
     <div class="divFormulario">
-        <form class="formularioLogin" method="POST">
+        <form id="newBus" method="POST">
             <div class="divTituloLogin">
                 <h4>Actualizacion de datos del bus</h4>
             </div>
-            <input type="text" class="form-control" name="id_bus" id="id_bus" value="<?php echo $id_bus ?>" hidden>
+            <input type="text" class="form-control" name="id_bus" id="id_bus" value="<?php echo $id_bus ?>" readonly>
             <div class="mb-3">
                 <label for="numero_bus" class="form-label" style="font-weight:bold;">Numero Bus</label>
                 <input type="text" class="form-control" name="numero_bus" id="numero_bus" placeholder="Numero Bus"
@@ -33,26 +65,31 @@ $estado = $_GET['estado'];
                 <input type="text" class="form-control" name="placa_bus" id="placa_bus" placeholder="Numero de Placa"
                     value="<?php echo $placa_bus ?>" required>
             </div>
+            
+            <div class="mb-3">
+                <label for="chasis_bus" class="form-label" style="font-weight:bold;">Chasis</label>
+                <input type="text" class="form-control" name="chasis_bus" id="chasis_bus" placeholder="Numero de Placa"
+                    value="<?php echo $chasis_bus ?>" required>
+            </div>
             <div class="mb-3">
                 <label for="carroceria_bus" class="form-label" style="font-weight:bold;">Carroceria</label>
                 <input type="text" class="form-control" name="carroceria_bus" id="carroceria_bus"
                     placeholder="Carroceria" value="<?php echo $carroceria_bus ?>" required>
             </div>
             <div class="mb-3">
-                <label for="placa_bus" class="form-label" style="font-weight:bold;">Numero de Placa</label>
-                <input type="text" class="form-control" name="placa_bus" id="placa_bus" placeholder="Numero de Placa"
-                    value="<?php echo $placa_bus ?>" required>
-            </div>
-            <div class="mb-3">
                 <label for="cantidad_asientos" class="form-label" style="font-weight:bold;">Numero de asientos</label>
-                <input type="number" min="30" class="form-control" value="<?php echo $cantidad_asientos ?>"
-                    name="cantidad_asientos" id="cantidad_asientos" required>
+                <input type="text" class="form-control" value="<?php echo $cantidad_asientos ?>"
+                    name="cantidad_asientos" id="cantidad_asientos" readonly>
             </div>
             <div class="mb-3">
                 <label for="id_socio" class="form-label" style="font-weight:bold;">Socio</label>
                 <input type="text" class="form-control" name="id_socio" id="id_socio" value="<?php echo $id_socio ?>"
                     readonly>
             </div>
+            <div class="mb-3">
+        <label for="fotografia" class="form-label" style="font-weight:bold;">Fotografia</label>
+        <input type="text" class="form-control" name="fotografia" id="fotografia" value="<?php echo $fotografia ?>" placeholder="Fotografia" required>
+      </div>
             <div class="mb-3">
                 <label for="estado" class="form-label" style="font-weight:bold;">Estado</label>
                 <select class="form-control" name="estado" id="estado">
@@ -63,7 +100,7 @@ $estado = $_GET['estado'];
                     </select>
                 </div>
                 <div>
-                    <button type="submit" class="btn btn-primary" id="envio" name="envio">Registrar</button>
+                    <button type="submit" class="btn btn-primary" id="envio" onclick="redirectToBuses()"  name="envio">Registrar</button>
                     <button type="button" class="btn btn-danger"><a
                             href="redireccionoficinista.php?action=buses">Cancelar</a></button>
                 </div>
