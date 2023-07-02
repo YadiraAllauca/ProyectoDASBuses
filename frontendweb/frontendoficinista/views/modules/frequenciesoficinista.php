db01f6bb358f899e1bb9fe027e3f7efde492dd42
Eldinosaur <eldinosaur7u7@outlook.com>
2023-07-01 20:58:39 -0500

    envio datos a update de frecuencias

--- frontendweb/frontendoficinista/views/modules/frequenciesoficinista.php
+++ frontendweb/frontendoficinista/views/modules/frequenciesoficinista.php
@@ -50,10 +50,7 @@
                             } ?>
                         </td>
                         <td><a class="nav-link active; navTemplate"
-                                href="redireccionoficinista.php?action=updatefrequency&id_frecuencia=<?php echo $id_frecuencia;?>
-                                &origen_frecuencia=<?php echo $origen_frcuencia;?>&destino_frecuencia=<?php echo $destino_frecuencia?>
-                                &duracion_frecuencia=<?php echo $duracion_frecuencia?>&tipo_frecuencia=<?php echo $tipo_frecuencia?>
-                                &costo_frecuencia=<?php echo $costo_frecuencia?>&estado_frecuencia=<?php echo $estado_frecuencia?>">
+                                href="redireccionoficinista.php?action=updatefrequency&id_frecuencia=<?php echo $id_frecuencia;?>&origen_frecuencia=<?php echo $origen_frcuencia;?>&destino_frecuencia=<?php echo $destino_frecuencia?>&duracion_frecuencia=<?php echo $duracion_frecuencia?>&tipo_frecuencia=<?php echo $tipo_frecuencia?>&costo_frecuencia=<?php echo $costo_frecuencia?>&estado_frecuencia=<?php echo $estado_frecuencia?>">
                                 <img src="img/editar.png" class="icons">
                             </a></td>
                     </tr>
